08b587759f6931498224ab10143db25954ab52bf
Makoto <bomakoto@users.noreply.github.com>
2019-04-19 15:53:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-var ad = document.getElementsByClassName("ndPaywall");
-ad[0].style.display="none";
-
-var eles = ["ndArticle_headPic", "ndArticle_margin"];
-for(var i=0, len=eles.length; i<len; i++){
-	var ele = document.getElementsByClassName(eles[i]);
-	if(ele[0] == undefined){
-		continue;
-	}
-	ele[0].style.display="";
-}
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('script.js');
+(document.head || document.documentElement).appendChild(s);
